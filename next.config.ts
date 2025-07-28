b9648b4830c7d298868561eb4503e82472b0a794
Afes <afesdaller@gmail.com>
2025-07-28 07:09:22 +0300

    update build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
+    basePath: "/dZENcode",
+    assetPrefix: "/dZENcode/",
     /* config options here */
 };
 
